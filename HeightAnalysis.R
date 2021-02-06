390872e5b004c1b9221b6e416a9559c4ced2efa8
TrevorHD <44789861+TrevorHD@users.noreply.github.com>
2021-02-06 10:14:34 -0500

    Figure updates 3
    Added code for CCDF and CCDF ratio figures.

--- HeightAnalysis.R
+++ HeightAnalysis.R
@@ -415,7 +415,7 @@ WALD.h <- function(n, h){
 
 
 
-##### Plot dispersal kernels: warmed vs not warmed --------------------------------------------------------
+##### Plot dispersal kernels: warmed vs not warmed PDF ----------------------------------------------------
 
 # Set up function to create a single sample and estimate PDF
 WALD.hBoot <- function(heights){
@@ -500,6 +500,178 @@ dev.off()
 ks.test(hBoot_dat_WNW1[, 2], hBoot_dat_WNW1[, 5], alt = "two.sided")
 ks.test(hBoot_dat_WNW2[, 2], hBoot_dat_WNW2[, 5], alt = "two.sided")
 
+
+
+
+
+##### Plot dispersal kernels: warmed vs not warmed CCDF ---------------------------------------------------
+
+# Set up function to create a single sample and estimate CCDF
+WALD.hBootCCDF <- function(heights){
+  hList <- sort(WALD.h(100, heights))
+  hList.exceed <- function(cutoff){
+    return(length(hList[hList >= cutoff])/length(hList))}
+  hList.ccdf <- approxfun(hList, sapply(hList, hList.exceed), yleft = 1, yright = 0)
+  return(hList.ccdf(seq(0, 60, by = 0.01)))}
+
+# Function to bootstrap CCDF and estimate 95% bootstrap interval
+# For each CCDF sample 100 release heights with 100 seed releases each, and repeat 1000 times
+WNW.CCDF <- function(NWHeight, WHeight){
+  hBoot_1 <- data.frame(replicate(1000, WALD.hBootCCDF(NWHeight), simplify = "matrix"))
+  hBoot_2 <- data.frame(replicate(1000, WALD.hBootCCDF(WHeight), simplify = "matrix"))
+  return(data.frame(seq(0, 60, by = 0.01),
+                    apply(X = hBoot_1, MARGIN = 1, FUN = mean),
+                    apply(X = hBoot_1, MARGIN = 1, FUN = quantile, probs = 0.025),
+                    apply(X = hBoot_1, MARGIN = 1, FUN = quantile, probs = 0.975),
+                    apply(X = hBoot_2, MARGIN = 1, FUN = mean),
+                    apply(X = hBoot_2, MARGIN = 1, FUN = quantile, probs = 0.025),
+                    apply(X = hBoot_2, MARGIN = 1, FUN = quantile, probs = 0.975)))}
+
+# Function to plot bootstrapped CCDF, with mean line and 95% band
+WNW.CCDFPlot <- function(WNW_data, bottom){
+  if(bottom == FALSE){
+    plot(x = WNW_data[, 1], y = WNW_data[, 2], type = "l", xlim = c(0, 60), ylim = c(0, 1),
+         xaxt = "n", ylab = "Complement of Cumulative Probability")
+    axis(side = 1, at = seq(0, 60, by = 10), labels = FALSE)}
+  if(bottom == TRUE){
+    plot(x = WNW_data[, 1], y = WNW_data[, 2], type = "l", xlim = c(0, 60), ylim = c(0, 1),
+         xlab = "Dispersal Distance (m)", ylab = "Complement of Cumulative Probability")}
+  polygon(x = c(WNW_data[, 1], rev(WNW_data[, 1])), 
+          y = c(WNW_data[, 3], rev(WNW_data[, 4])), col = alpha("black", alpha = 0.2), border = NA)
+  lines(x = WNW_data[, 1], y = WNW_data[, 5], type = "l", col = "red")
+  polygon(x = c(WNW_data[, 1], rev(WNW_data[, 1])), 
+          y = c(WNW_data[, 6], rev(WNW_data[, 7])), col = alpha("red", alpha = 0.2), border = NA)}
+
+# Prepare graphics device
+jpeg(filename = "Figure 3.1.jpeg", width = 826, height = 568, units = "px")
+
+# Create blank page
+grid.newpage()
+plot.new()
+
+# Set grid layout and activate it
+gly <- grid.layout(1200, 800)
+pushViewport(viewport(layout = gly))
+
+# CN non-warmed vs warmed
+pushViewport(vp = viewport(layout.pos.row = 1:600, layout.pos.col = 1:800))
+par(fig = gridFIG())
+par(new = TRUE)
+par(mar = c(2, 4, 2, 1))
+hBoot_dat_WNW1CCDF <- WNW.CCDF(ht_CN_NW$Height, ht_CN_W$Height)
+WNW.CCDFPlot(hBoot_dat_WNW1CCDF, bottom = FALSE)
+popViewport()
+
+# CA non-warmed vs warmed
+pushViewport(vp = viewport(layout.pos.row = 600:1200, layout.pos.col = 1:800))
+par(fig = gridFIG())
+par(new = TRUE)
+par(mar = c(4, 4, 0, 1))
+hBoot_dat_WNW2CCDF <- WNW.CCDF(ht_CA_NW$Height, ht_CA_W$Height)
+WNW.CCDFPlot(hBoot_dat_WNW2CCDF, bottom = TRUE)
+popViewport()
+
+# Create legend
+grid.text(label = c("Warmed", "Not Warmed"), x = rep(0.84, 2), y = c(0.87, 0.90),
+          hjust = 0, gp = gpar(cex = 0.8))
+grid.segments(x0 = rep(0.813, 2), y0 = c(0.868, 0.898), x1 = rep(0.833, 2), y1 = c(0.868, 0.898),
+              gp = gpar(col = c("red", "black")))
+
+# Create figure labels
+grid.text(label = c("A", "B"), x = rep(0.11, 2), y = c(0.90, 0.47),
+          hjust = 0, gp = gpar(cex = 1.1))
+popViewport()
+
+# Deactivate grid layout; finalise graphics save
+popViewport()
+dev.off()
+
+# Kolmogorov-Smirnov test for NW/W CN and NW/W CA
+# The NW and W dispersal kernels display significant difference for CN and CA
+ks.test(hBoot_dat_WNW1[, 2], hBoot_dat_WNW1[, 5], alt = "two.sided")
+ks.test(hBoot_dat_WNW2[, 2], hBoot_dat_WNW2[, 5], alt = "two.sided")
+
+
+
+
+
+##### Plot dispersal kernels: warmed vs not warmed CCDF ratio ---------------------------------------------
+
+# Function to bootstrap CCDF and estimate 95% bootstrap interval
+# For each CCDF sample 100 release heights with 100 seed releases each, and repeat 1000 times
+WNW.CCDFRatio <- function(NWHeight, WHeight){
+  hBoot_1 <- data.frame(replicate(1000, WALD.hBootCCDF(NWHeight), simplify = "matrix"))
+  hBoot_2 <- data.frame(replicate(1000, WALD.hBootCCDF(WHeight), simplify = "matrix"))
+  hBoot_a <- hBoot_2/hBoot_1
+  mean.exinf <- function(x){
+    return(mean(x[!is.infinite(x)]))}
+  quantile.exinf <- function(x, probs){
+    return(quantile(x[!is.infinite(x)], probs = probs))}
+  return(data.frame(seq(0, 60, by = 0.01),
+                    apply(X = hBoot_a, MARGIN = 1, FUN = mean.exinf),
+                    apply(X = hBoot_a, MARGIN = 1, FUN = quantile.exinf, probs = 0.025),
+                    apply(X = hBoot_a, MARGIN = 1, FUN = quantile.exinf, probs = 0.975)))}
+
+# Function to plot bootstrapped CCDF, with mean line and 95% band
+WNW.CCDFRatioPlot <- function(WNW_data, bottom){
+  if(bottom == FALSE){
+    plot(x = WNW_data[, 1], y = WNW_data[, 2], type = "l", xlim = c(0, 60), ylim = c(0, 4),
+         xaxt = "n", ylab = "W/NW CCDF Ratio")
+    axis(side = 1, at = seq(0, 60, by = 10), labels = FALSE)}
+  if(bottom == TRUE){
+    plot(x = WNW_data[, 1], y = WNW_data[, 2], type = "l", xlim = c(0, 60), ylim = c(0, 4),
+         xlab = "Dispersal Distance (m)", ylab = "W/NW CCDF Ratio")}
+  polygon(x = c(WNW_data[, 1], rev(WNW_data[, 1])), 
+          y = c(WNW_data[, 3], rev(WNW_data[, 4])), col = alpha("black", alpha = 0.2), border = NA)}
+
+# Prepare graphics device
+jpeg(filename = "Figure 3.2.jpeg", width = 826, height = 568, units = "px")
+
+# Create blank page
+grid.newpage()
+plot.new()
+
+# Set grid layout and activate it
+gly <- grid.layout(1200, 800)
+pushViewport(viewport(layout = gly))
+
+# CN non-warmed vs warmed
+pushViewport(vp = viewport(layout.pos.row = 1:600, layout.pos.col = 1:800))
+par(fig = gridFIG())
+par(new = TRUE)
+par(mar = c(2, 4, 2, 1))
+hBoot_dat_WNW1CCDFRatio <- WNW.CCDFRatio(ht_CN_NW$Height, ht_CN_W$Height)
+WNW.CCDFRatioPlot(hBoot_dat_WNW1CCDFRatio, bottom = FALSE)
+popViewport()
+
+# CA non-warmed vs warmed
+pushViewport(vp = viewport(layout.pos.row = 600:1200, layout.pos.col = 1:800))
+par(fig = gridFIG())
+par(new = TRUE)
+par(mar = c(4, 4, 0, 1))
+hBoot_dat_WNW2CCDFRatio <- WNW.CCDFRatio(ht_CA_NW$Height, ht_CA_W$Height)
+WNW.CCDFRatioPlot(hBoot_dat_WNW2CCDFRatio, bottom = TRUE)
+popViewport()
+
+# Create legend
+grid.text(label = c("Warmed", "Not Warmed"), x = rep(0.84, 2), y = c(0.87, 0.90),
+          hjust = 0, gp = gpar(cex = 0.8))
+grid.segments(x0 = rep(0.813, 2), y0 = c(0.868, 0.898), x1 = rep(0.833, 2), y1 = c(0.868, 0.898),
+              gp = gpar(col = c("red", "black")))
+
+# Create figure labels
+grid.text(label = c("A", "B"), x = rep(0.11, 2), y = c(0.90, 0.47),
+          hjust = 0, gp = gpar(cex = 1.1))
+popViewport()
+
+# Deactivate grid layout; finalise graphics save
+popViewport()
+dev.off()
+
+# Kolmogorov-Smirnov test for NW/W CN and NW/W CA
+# The NW and W dispersal kernels display significant difference for CN and CA
+ks.test(hBoot_dat_WNW1[, 2], hBoot_dat_WNW1[, 5], alt = "two.sided")
+ks.test(hBoot_dat_WNW2[, 2], hBoot_dat_WNW2[, 5], alt = "two.sided")
 
 
 
@@ -701,6 +873,209 @@ ks.test(hBoot_dat_MHD1[, 2], hBoot_dat_MHD1[, 5], alt = "two.sided")
 ks.test(hBoot_dat_MHD2[, 2], hBoot_dat_MHD2[, 5], alt = "two.sided")
 ks.test(hBoot_dat_MHD3[, 2], hBoot_dat_MHD3[, 5], alt = "two.sided")
 ks.test(hBoot_dat_MHD4[, 2], hBoot_dat_MHD4[, 5], alt = "two.sided")
+
+
+
+
+
+##### Plot dispersal kernels: max height vs height distribution CCDF --------------------------------------
+
+# Set up function to create a single sample and estimate CCDF
+WALD.hBootCCDF <- function(heights){
+  hList <- sort(WALD.h(100, heights))
+  hList.exceed <- function(cutoff){
+    return(length(hList[hList >= cutoff])/length(hList))}
+  hList.ccdf <- approxfun(hList, sapply(hList, hList.exceed), yleft = 1, yright = 0)
+  return(hList.ccdf(seq(0, 60, by = 0.01)))}
+
+# Function to bootstrap CCDF and estimate 95% bootstrap interval
+# For each CCDF sample 100 release heights with 100 seed releases each, and repeat 1000 times
+MHD.CCDF <- function(distHeight, maxHeight){
+  hBoot_1 <- data.frame(replicate(1000, WALD.hBootCCDF(distHeight), simplify = "matrix"))
+  hBoot_2 <- data.frame(replicate(1000, WALD.hBootCCDF(maxHeight), simplify = "matrix"))
+  return(data.frame(seq(0, 60, by = 0.01),
+                    apply(X = hBoot_1, MARGIN = 1, FUN = mean),
+                    apply(X = hBoot_1, MARGIN = 1, FUN = quantile, probs = 0.025),
+                    apply(X = hBoot_1, MARGIN = 1, FUN = quantile, probs = 0.975),
+                    apply(X = hBoot_2, MARGIN = 1, FUN = mean),
+                    apply(X = hBoot_2, MARGIN = 1, FUN = quantile, probs = 0.025),
+                    apply(X = hBoot_2, MARGIN = 1, FUN = quantile, probs = 0.975)))}
+
+# Plot the dispersal percentiles and the corresponding distances
+# Error bars indicate the range in which 95% of the bootstrapped simulations are
+# Lines indicate mean values
+MHD.rtailPlot <- function(rtail_data, LColour, PColour, bNum, bLab){
+  if(bNum == FALSE){
+    plot(x = rtail_data[, 1], y = rtail_data[, 4], type = "l", col = LColour, xlim = c(0, 60), ylim = c(0, 1),
+         xaxt = "n", ylab = "Dispersal Distance (m)")
+    axis(side = 1, at = seq(0, 60, by = 10), labels = FALSE)}
+  if(bNum == TRUE){
+    plot(x = rtail_data[, 1], y = rtail_data[, 4], type = "l", col = LColour, xlim = c(0, 60), ylim = c(0, 1),
+         xlab = ifelse(bLab == TRUE, "Dispersal Quantile", NA), ylab = "Dispersal Distance (m)")}
+  polygon(x = c(rtail_data[, 1], rev(rtail_data[, 1])), 
+          y = c(rtail_data[, 5], rev(rtail_data[, 6])), col = alpha(LColour, alpha = 0.2), border = NA)
+  lines(x = rtail_data[, 1], y = rtail_data[, 4], type = "l", lty = 3, col = PColour)
+  polygon(x = c(rtail_data[, 1], rev(rtail_data[, 1])), 
+          y = c(rtail_data[, 5], rev(rtail_data[, 6])), col = alpha(PColour, alpha = 0.2), border = NA)}
+
+# Prepare graphics device
+jpeg(filename = "Figure 6.jpeg", width = 826, height = 825, units = "px")
+
+# Create blank page
+grid.newpage()
+plot.new()
+
+# Set grid layout and activate it
+gly <- grid.layout(2400, 800)
+pushViewport(viewport(layout = gly))
+
+# CN non-warmed: mean vs distribution
+pushViewport(vp = viewport(layout.pos.row = 1:600, layout.pos.col = 1:800))
+par(fig = gridFIG())
+par(new = TRUE)
+par(mar = c(1.1, 4, 2.9, 1))
+hBoot_dat_rtMHD1 <- MHD.CCDF(ht_CN_NW$Height, ht_CN_NW_max$max)
+MHD.rtailPlot(hBoot_dat_rtMHD1, "black", "gray48", bNum = FALSE, bLab = FALSE)
+popViewport()
+
+# CN warmed: mean vs distribution
+pushViewport(vp = viewport(layout.pos.row = 600:1200, layout.pos.col = 1:800))
+par(fig = gridFIG())
+par(new = TRUE)
+par(mar = c(3, 4, 1, 1))
+hBoot_dat_rtMHD2 <- MHD.CCDF(ht_CN_W$Height, ht_CN_W_max$max)
+MHD.rtailPlot(hBoot_dat_rtMHD2, "red2", "indianred1", bNum = TRUE, bLab = FALSE)
+popViewport()
+
+# CA non-warmed: mean vs distribution
+pushViewport(vp = viewport(layout.pos.row = 1200:1800, layout.pos.col = 1:800))
+par(fig = gridFIG())
+par(new = TRUE)
+par(mar = c(2, 4, 2, 1))
+hBoot_dat_rtMHD3 <- MHD.CCDF(ht_CA_NW$Height, ht_CA_NW_max$max)
+MHD.rtailPlot(hBoot_dat_rtMHD3, "black", "gray48", bNum = FALSE, bLab = FALSE)
+popViewport()
+
+# CA warmed: mean vs distribution
+pushViewport(vp = viewport(layout.pos.row = 1800:2400, layout.pos.col = 1:800))
+par(fig = gridFIG())
+par(new = TRUE)
+par(mar = c(4, 4, 0, 1))
+hBoot_dat_rtMHD4 <- MHD.CCDF(ht_CA_W$Height, ht_CA_W_max$max)
+MHD.rtailPlot(hBoot_dat_rtMHD4, "red2", "indianred1", bNum = TRUE, bLab = TRUE)
+popViewport()
+
+# Create legend
+grid.text(label = c("Dist. Height (W)", "Dist. Height (NW)", "Max Height (W)", "Max Height (NW)"), 
+          x = rep(0.83, 4), y = c(0.86, 0.88, 0.90 , 0.92), hjust = 0, gp = gpar(cex = 0.8))
+grid.segments(x0 = rep(0.803, 4), y0 = c(0.857, 0.877, 0.897 , 0.917), 
+              x1 = rep(0.823, 4), y1 = c(0.857, 0.877, 0.897 , 0.917),
+              gp = gpar(col = rep(c("red2", "black"), 2), lty = c(1, 1, 3, 3)))
+
+# Create figure labels
+grid.text(label = c("A", "B", "C", "D"), x = rep(0.11, 4), y = c(0.92, 0.71, 0.44, 0.23),
+          hjust = 0, gp = gpar(cex = 1.1))
+popViewport()
+
+# Deactivate grid layout; finalise graphics save
+popViewport()
+dev.off()
+
+
+
+
+
+##### Plot dispersal kernels: warmed vs not warmed CCDF ratio ---------------------------------------------
+
+# Function to bootstrap CCDF and estimate 95% bootstrap interval
+# For each CCDF sample 100 release heights with 100 seed releases each, and repeat 1000 times
+MHD.CCDFRatio <- function(DistH, MaxH){
+  hBoot_1 <- data.frame(replicate(1000, WALD.hBootCCDF(DistH), simplify = "matrix"))
+  hBoot_2 <- data.frame(replicate(1000, WALD.hBootCCDF(MaxH), simplify = "matrix"))
+  hBoot_a <- hBoot_2/hBoot_1
+  mean.exinf <- function(x){
+    return(mean(x[!is.infinite(x)]))}
+  quantile.exinf <- function(x, probs){
+    return(quantile(x[!is.infinite(x)], probs = probs))}
+  return(data.frame(seq(0, 60, by = 0.01),
+                    apply(X = hBoot_a, MARGIN = 1, FUN = mean.exinf),
+                    apply(X = hBoot_a, MARGIN = 1, FUN = quantile.exinf, probs = 0.025),
+                    apply(X = hBoot_a, MARGIN = 1, FUN = quantile.exinf, probs = 0.975)))}
+
+# Function to plot bootstrapped CCDF, with mean line and 95% band
+MHD.CCDFRatioPlot <- function(rtail_data, bNum, bLab){
+  if(bNum == FALSE){
+    plot(x = rtail_data[, 1], y = rtail_data[, 2], type = "l", xlim = c(0, 60), ylim = c(0, 4),
+         xaxt = "n", ylab = "M/HD CCDF Ratio")
+    axis(side = 1, at = seq(0, 60, by = 10), labels = FALSE)}
+  if(bNum == TRUE){
+    plot(x = rtail_data[, 1], y = rtail_data[, 2], type = "l", xlim = c(0, 60), ylim = c(0, 4),
+         xlab = ifelse(bLab == TRUE, "Dispersal Distance (m)", NA), ylab = "M/HD CCDF Ratio")}
+  polygon(x = c(rtail_data[, 1], rev(rtail_data[, 1])), 
+          y = c(rtail_data[, 3], rev(rtail_data[, 4])), col = alpha("black", alpha = 0.2), border = NA)}
+
+# Prepare graphics device
+jpeg(filename = "Figure 6.jpeg", width = 826, height = 825, units = "px")
+
+# Create blank page
+grid.newpage()
+plot.new()
+
+# Set grid layout and activate it
+gly <- grid.layout(2400, 800)
+pushViewport(viewport(layout = gly))
+
+# CN non-warmed: mean vs distribution
+pushViewport(vp = viewport(layout.pos.row = 1:600, layout.pos.col = 1:800))
+par(fig = gridFIG())
+par(new = TRUE)
+par(mar = c(1.1, 4, 2.9, 1))
+hBoot_dat_rtMHD1 <- MHD.CCDFRatio(ht_CN_NW$Height, ht_CN_NW_max$max)
+MHD.CCDFRatioPlot(hBoot_dat_rtMHD1, bNum = FALSE, bLab = FALSE)
+popViewport()
+
+# CN warmed: mean vs distribution
+pushViewport(vp = viewport(layout.pos.row = 600:1200, layout.pos.col = 1:800))
+par(fig = gridFIG())
+par(new = TRUE)
+par(mar = c(3, 4, 1, 1))
+hBoot_dat_rtMHD2 <- MHD.CCDFRatio(ht_CN_W$Height, ht_CN_W_max$max)
+MHD.CCDFRatioPlot(hBoot_dat_rtMHD2, bNum = TRUE, bLab = FALSE)
+popViewport()
+
+# CA non-warmed: mean vs distribution
+pushViewport(vp = viewport(layout.pos.row = 1200:1800, layout.pos.col = 1:800))
+par(fig = gridFIG())
+par(new = TRUE)
+par(mar = c(2, 4, 2, 1))
+hBoot_dat_rtMHD3 <- MHD.CCDFRatio(ht_CA_NW$Height, ht_CA_NW_max$max)
+MHD.CCDFRatioPlot(hBoot_dat_rtMHD3, bNum = FALSE, bLab = FALSE)
+popViewport()
+
+# CA warmed: mean vs distribution
+pushViewport(vp = viewport(layout.pos.row = 1800:2400, layout.pos.col = 1:800))
+par(fig = gridFIG())
+par(new = TRUE)
+par(mar = c(4, 4, 0, 1))
+hBoot_dat_rtMHD4 <- MHD.CCDFRatio(ht_CA_W$Height, ht_CA_W_max$max)
+MHD.CCDFRatioPlot(hBoot_dat_rtMHD4, bNum = TRUE, bLab = TRUE)
+popViewport()
+
+# Create legend
+grid.text(label = c("Dist. Height (W)", "Dist. Height (NW)", "Max Height (W)", "Max Height (NW)"), 
+          x = rep(0.83, 4), y = c(0.86, 0.88, 0.90 , 0.92), hjust = 0, gp = gpar(cex = 0.8))
+grid.segments(x0 = rep(0.803, 4), y0 = c(0.857, 0.877, 0.897 , 0.917), 
+              x1 = rep(0.823, 4), y1 = c(0.857, 0.877, 0.897 , 0.917),
+              gp = gpar(col = rep(c("red2", "black"), 2), lty = c(1, 1, 3, 3)))
+
+# Create figure labels
+grid.text(label = c("A", "B", "C", "D"), x = rep(0.11, 4), y = c(0.92, 0.71, 0.44, 0.23),
+          hjust = 0, gp = gpar(cex = 1.1))
+popViewport()
+
+# Deactivate grid layout; finalise graphics save
+popViewport()
+dev.off()
 
 
 
