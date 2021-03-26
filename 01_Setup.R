3065ac507974a0a60f382e5cff79d45358646ffb
TrevorHD <44789861+TrevorHD@users.noreply.github.com>
2021-03-26 17:04:47 -0400

    Figure updates 6
    Added figure of individual bootstrap replicates for the flower height distributions, including the linear mean and Vincent average.

--- 01_Setup.R
+++ 01_Setup.R
@@ -149,25 +149,7 @@ WALD.h <- function(n, h){
 
 ##### [WIP] Calculate Vincent average rather than average on linear pool ----------------------------------
 
-# Calculate Vincent averages for height distribution
-mean.vincent <- function(hBoot){
-  
-  # Calculate CDF from PDF
-  cdf <- function(coldata){
-    return(cumsum(coldata/sum(coldata)))}
-  cdf_values <- as.data.frame(apply(X = hBoot, MARGIN = 2, FUN = cdf))
-  
-  # Get height for each quantile
-  quantiles <- function(coldata){
-    quantile_data <- data.frame(cbind(seq(0, 200, by = 0.1), coldata))
-    return(approx(quantile_data[, 2], quantile_data[, 1], seq(0, 1, by = 0.0001), yleft = 0, yright = 200)$y)}
-  
-  # Calculate Vincent average for heights across each quantile
-  cdf_quantiles <- data.frame(apply(X = cdf_values, MARGIN = 2, FUN = quantiles))
-  cdf_quantiles <- apply(X = cdf_quantiles, MARGIN = 1, FUN = mean)
-  
-  # Return average distribution   
-  return(cdf_quantiles)}
+
 
 # Test plotting for Vincent average (red) versus linear pooling (blue)
 # Fit seems to be slightly off; need to fix this
@@ -193,12 +175,12 @@ mean.vincent <- function(hBoot){
     return(cumsum(coldata/sum(coldata)))}
   cdf_values <- as.data.frame(apply(X = hBoot, MARGIN = 2, FUN = cdf))
   
-  # Get height for each quantile
+  # Get distance for each quantile
   quantiles <- function(coldata){
     quantile_data <- data.frame(cbind(seq(0, 8, by = 0.01), coldata))
     return(approx(quantile_data[, 2], quantile_data[, 1], seq(0, 1, by = 0.0001), yleft = 0, yright = 8)$y)}
   
-  # Calculate Vincent average for heights across each quantile
+  # Calculate Vincent average for distances across each quantile
   cdf_quantiles <- data.frame(apply(X = cdf_values, MARGIN = 2, FUN = quantiles))
   cdf_quantiles <- apply(X = cdf_quantiles, MARGIN = 1, FUN = mean)
   
@@ -215,8 +197,8 @@ plot(seq(0, 8, by = 0.01), hBoot_1[, 1], type = "l", ylim = c(0, 0.5), xlab = "D
      ylab = "Probability Density", col = rgb(red = 0, green = 0, blue = 0, alpha = 0.03))
 for(i in 2:1000){
   lines(seq(0, 8, by = 0.01), hBoot_1[, i], col = rgb(red = 0, green = 0, blue = 0, alpha = 0.03))}
-lines(density(mean.vincent(hBoot_1), from = 0, to = 8, bw = 0.05), col = "red", lwd = 1.8)
-lines(seq(0, 8, by = 0.01), apply(X = hBoot_1, MARGIN = 1, FUN = mean), col = "lightblue", lwd = 1.8)
+lines(density(mean.vincent(hBoot_1), from = 0.04, to = 8, bw = 0.05), col = "red", lwd = 1.8)
+#lines(seq(0, 8, by = 0.01), apply(X = hBoot_1, MARGIN = 1, FUN = mean), col = "lightblue", lwd = 1.8)
 
 
 
@@ -254,7 +236,7 @@ remove(HeightList)
 
 
 
-##### [F2] Estimate height distribution PDF with 95% bootstrap interval -----------------------------------
+##### [F2.1] Estimate height distribution PDF with 95% bootstrap interval ---------------------------------
 
 # Set up function to estimate PDF for a bootstrapped sample
 ht.pdf <- function(heights){
@@ -294,6 +276,69 @@ ht.pdfBoot.plot <- function(bootData, bottom){
 # Bootstrap PDFs for NW/W CN and NW/W CA heights
 hBoot_HD1 <- ht.pdfBoot(ht_CN_NW$Height, ht_CN_W$Height)
 hBoot_HD2 <- ht.pdfBoot(ht_CA_NW$Height, ht_CA_W$Height)
+
+
+
+
+
+##### [F2.2] Same as above, but plot individual replicates and include Vincent average --------------------
+
+# Calculate Vincent averages for height distribution
+mean.vincent <- function(hBoot){
+  cdf <- function(coldata){
+    return(cumsum(coldata/sum(coldata)))}
+  cdf_values <- as.data.frame(apply(X = hBoot, MARGIN = 2, FUN = cdf))
+  quantiles <- function(coldata){
+    quantile_data <- data.frame(cbind(seq(0, 200, by = 0.1), coldata))
+    return(approx(quantile_data[, 2], quantile_data[, 1], seq(0, 1, by = 0.0001), yleft = 0, yright = 200)$y)}
+  cdf_quantiles <- data.frame(apply(X = cdf_values, MARGIN = 2, FUN = quantiles))
+  cdf_quantiles <- apply(X = cdf_quantiles, MARGIN = 1, FUN = mean) 
+  return(cdf_quantiles)}
+
+ht.pdfBoot2 <- function(h1, h2){
+  hBoot_1 <- data.frame(replicate(1000, ht.pdf(sample(h1, size = 500, replace = TRUE))))
+  hBoot_2 <- data.frame(replicate(1000, ht.pdf(sample(h2, size = 500, replace = TRUE))))
+  return(list(data.frame(seq(0, 200, by = 0.1),
+                    apply(X = hBoot_1, MARGIN = 1, FUN = mean),
+                    apply(X = hBoot_2, MARGIN = 1, FUN = mean)),
+              hBoot_1, hBoot_2,
+              mean.vincent(hBoot_1),
+              mean.vincent(hBoot_2)))}
+
+# Function to plot bootstrapped height distributions
+# Includes linear pool (blue) and Vincent average (yellow)
+ht.pdfBoot.plot2 <- function(bootData, bottom){
+  if(bottom == FALSE){
+    plot(x = bootData[[1]][, 1], y = bootData[[2]][, 1], type = "l", xlim = c(0, 200), ylim = c(0, 0.03),
+         xaxt = "n", yaxt = "n", ylab = "Probability Density",
+         col = rgb(red = 0, green = 0, blue = 0, alpha = 0.03))
+    lines(bootData[[1]][, 1], bootData[[3]][, 1], col = rgb(red = 1, green = 0.23, blue = 0.23, alpha = 0.03))
+    for(i in 2:1000){
+      lines(bootData[[1]][, 1], bootData[[2]][, i], col = rgb(red = 0, green = 0, blue = 0, alpha = 0.03))
+      lines(bootData[[1]][, 1], bootData[[3]][, i], col = rgb(red = 1, green = 0.23, blue = 0.23, alpha = 0.03))}
+    lines(density(bootData[[4]], from = 0, to = 200), col = "gold", lwd = 1.8)
+    lines(density(bootData[[5]], from = 0, to = 200), col = "gold", lwd = 1.8)
+    lines(bootData[[1]][, 1], bootData[[1]][, 2], col = "deepskyblue", lwd = 1.8)
+    lines(bootData[[1]][, 1], bootData[[1]][, 3], col = "deepskyblue", lwd = 1.8)
+    axis(side = 1, at = c(0, 50, 100, 150, 200), labels = FALSE)
+    axis(side = 2, at = c(0, 0.01, 0.02, 0.03), labels = TRUE)}
+  if(bottom == TRUE){
+    plot(x = bootData[[1]][, 1], y = bootData[[2]][, 1], type = "l", xlim = c(0, 200), ylim = c(0, 0.03),
+         xlab = "Flower Height (cm)", yaxt = "n", ylab = "Probability Density",
+         col = rgb(red = 0.4, green = 0, blue = 0, alpha = 0.03))
+    lines(bootData[[1]][, 1], bootData[[3]][, 1], col = rgb(red = 1, green = 0.23, blue = 0.23, alpha = 0.03))
+    for(i in 2:1000){
+      lines(bootData[[1]][, 1], bootData[[2]][, i], col = rgb(red = 0, green = 0, blue = 0, alpha = 0.03))
+      lines(bootData[[1]][, 1], bootData[[3]][, i], col = rgb(red = 1, green = 0.23, blue = 0.23, alpha = 0.03))}
+    lines(density(bootData[[4]], from = 0, to = 200), col = "gold", lwd = 1.8)
+    lines(density(bootData[[5]], from = 0, to = 200), col = "gold", lwd = 1.8)
+    lines(bootData[[1]][, 1], bootData[[1]][, 2], col = "deepskyblue", lwd = 1.8)
+    lines(bootData[[1]][, 1], bootData[[1]][, 3], col = "deepskyblue", lwd = 1.8)
+    axis(side = 2, at = c(0, 0.01, 0.02, 0.03), labels = TRUE)}}
+
+# Bootstrap PDFs for NW/W CN and NW/W CA heights
+hBoot_HD1_2 <- ht.pdfBoot2(ht_CN_NW$Height, ht_CN_W$Height)
+hBoot_HD2_2 <- ht.pdfBoot2(ht_CA_NW$Height, ht_CA_W$Height)
 
 
 
