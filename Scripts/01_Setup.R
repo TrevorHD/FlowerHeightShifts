a17ed1c7eb1908ac1d6aa7da0e6a8b60e77c44cb
TrevorHD <44789861+TrevorHD@users.noreply.github.com>
2021-06-23 21:36:05 -0400

    Miscellaneous fixes 10
    Added code to load in rosette data (for covariate in mixed-effect models) and fixed some minor issues with the plotting functions.

--- Scripts/01_Setup.R
+++ Scripts/01_Setup.R
@@ -11,16 +11,30 @@ library(lme4)
 library(lmerTest)
 
 # Load in raw weather data
-data_ws1 <- read.csv("../Data/Weather1.csv")
-data_ws2 <- read.csv("../Data/Weather2.csv")
+data_ws1 <- read.csv("Data/Weather1.csv")
+data_ws2 <- read.csv("Data/Weather2.csv")
 
-# Load in terminal velocity data
-# Use ambient TVs since we're only examining warming effects on height distribution
-data_tv <- read.delim("../Data/SeedDropData2.txt")
+# Load in terminal velocity data and remove rows with missing values
+data_tv <- read.delim("Data/SeedDropData2.txt")
 data_tv <- subset(data_tv, !is.na(drop.time))
 
 # Load height data as xlsx
-data_ht <- read.xlsx("../Data/ThistleData.xlsx", sheetName = "Flowers")
+data_ht <- read.xlsx("Data/ThistleData.xlsx", sheetName = "Flowers")
+
+# Load rosette as xlsx
+data_rs <- read.xlsx("Data/ThistleData.xlsx", sheetName = "General")
+
+
+
+
+
+##### Tidy data -------------------------------------------------------------------------------------------
+
+# Remove rosette data rows with dead or non-flowering individuals
+# Then merge initial rosette diameter with flower height data and keep only necessary columns
+data_rs <- subset(data_rs, !is.na(LLL_t1) & !is.na(DM_t1) & F == 1)
+data_ht <- merge(data_ht, data_rs, by = c("Row", "Group", "Plant", "Species"))
+data_ht <- data_ht[, 1:9]
 
 # Note: we are only including flowers (f) and seed heads (s), not buds (b)
 # We are also interested in only warmed (W) and not warmed (NW)
@@ -29,25 +43,25 @@ data_ht <- read.xlsx("../Data/ThistleData.xlsx", sheetName = "Flowers")
 # Examine heights for only flowers
 data_ht <- subset(data_ht, TRT != "PW" & (Type == "f" | Type == "s"))
 
-# Grab values for CN flower heights, for each treatment group
+# Create vector of CN flower heights for each treatment group
 ht_CN_NW <- subset(data_ht, Species == "CN" & TRT == "NW")
 ht_CN_W <- subset(data_ht, Species == "CN" & TRT == "W")
 
-# Grab values for CA flower heights, for each treatment group
+# Create vector of CA flower heights for each treatment group
 ht_CA_NW <- subset(data_ht, Species == "CA" & TRT == "NW")
 ht_CA_W <- subset(data_ht, Species == "CA" & TRT == "W")
 
-# Get maximum flower heights for each plant
+# Find maximum flower heights for each plant
 data_ht %>% 
   subset(Type == "f" | Type == "s") %>% 
   group_by(Row, Group, Plant, Species, TRT) %>% 
   summarise(max = max(Height)) -> data_ht_max
 
-# Grab values for CN max flower heights, for each treatment group
+# Create vector of CN max flower heights for each treatment group
 ht_CN_NW_max <- subset(data_ht_max, Species == "CN" & TRT == "NW")
 ht_CN_W_max <- subset(data_ht_max, Species == "CN" & TRT == "W")
 
-# Grab values for CA max flower heights, for each treatment group
+# Create vector of CA max flower heights for each treatment group
 ht_CA_NW_max <- subset(data_ht_max, Species == "CA" & TRT == "NW")
 ht_CA_W_max <- subset(data_ht_max, Species == "CA" & TRT == "W")
 
@@ -57,7 +71,7 @@ ht_CA_W_max <- subset(data_ht_max, Species == "CA" & TRT == "W")
 
 ##### Set up dispersal framework --------------------------------------------------------------------------
 
-# Create PDF of wind speeds
+# Create PDF of wind speeds at measurement height of 1 m
 # Assume no seed release occurs for wind speeds of zero, so remove zero values
 ws_values <- c(data_ws1$Wind1, data_ws2$Wind1)
 ws_values <- ws_values[ws_values > 0]
@@ -65,13 +79,13 @@ ws_pdf <- density(ws_values, from = min(ws_values), to = max(ws_values), bw = 0.
 ws_mean <- mean(ws_values)
 
 # Create PDF of CN wind speeds
-# Terminal velocity is drop tube length divided by drop time
+# Terminal velocity is drop tube length (1.25 m) divided by drop time
 tv_values_CN <- na.omit(1.25/subset(data_tv, species == "n")$drop.time)
 tv_pdf_CN <- density(tv_values_CN, from = min(tv_values_CN), to = max(tv_values_CN), bw = 0.05)
 tv_mean_CN <- mean(tv_values_CN)
 
 # Create PDF of CN wind speeds
-# Terminal velocity is drop tube length divided by drop time
+# Terminal velocity is drop tube length (1.25 m) divided by drop time
 tv_values_CA <- na.omit(1.25/subset(data_tv, species == "a")$drop.time)
 tv_pdf_CA <- density(tv_values_CA, from = min(tv_values_CA), to = max(tv_values_CA), bw = 0.05)
 tv_mean_CA <- mean(tv_values_CA)
@@ -86,10 +100,10 @@ WALD.b <- function(n, H, species){
   
   # Initialise other fixed quantities
   Aw <- 1.3     # Ratio of sigma_W to ustar
-  h <- 0.15     # Grass cover height
-  d <- 0.7*h    # Zero-plane displacement
-  z0 <- 0.1*h   # Roughness length
-  zm <- 1       # Wind speed measurement height
+  h <- 0.15     # Grass cover height (m)
+  d <- 0.7*h    # Zero-plane displacement (m)
+  z0 <- 0.1*h   # Roughness length (m)
+  zm <- 1       # Wind speed measurement height (m)
   
   # Let n be the number of simulation replications
   # Let H be the seed release height
@@ -217,7 +231,7 @@ ht.pdfBoot.plot <- function(bootData, bottom){
     plot(x = bootData[, 1], y = bootData[, 2], type = "l", xlim = c(0, 200), ylim = c(0, 0.03),
          xaxt = "n", yaxt = "n")
     axis(side = 1, at = c(0, 50, 100, 150, 200), labels = TRUE, mgp = c(0, 0, 0))
-    mtext(side = 1, line = 0.75, "Flower height (cm)", cex = 0.6)}
+    mtext(side = 1, line = 0.75, "Flower head height (cm)", cex = 0.6)}
   axis(side = 2, at = c(0, 0.01, 0.02, 0.03), labels = TRUE, mgp = c(0, 0.21, 0))
   mtext(side = 2, line = 0.95, "Probability density", cex = 0.6)
   polygon(x = c(bootData[, 1], rev(bootData[, 1])), 
@@ -494,21 +508,21 @@ ds.pdfBoot.plot2 <- function(bootData, LColour, PColour, position){
 # Plot the dispersal percentiles and the corresponding distances
 # Error bars indicate the range in which 95% of the bootstrapped simulations are
 # Lines indicate mean values
-ds.ccdfBoot.plot2 <- function(bootData, LColour, PColour, bottom){
+ds.ccdfBoot.plot2 <- function(bootData, LColour, PColour, position){
   par(cex.axis = 0.45, tcl = -0.2)
   if(position == "top"){
     par(mar = c(0.75, 1.75, 0.75, 0.75))
-    plot(x = bootData[, 1], y = bootData[, 4], type = "l", col = LColour, xlim = c(0, 60), ylim = c(0, 1),
+    plot(x = bootData[, 1], y = bootData[, 4], type = "l", col = LColour, xlim = c(0, 30), ylim = c(0, 1),
          xaxt = "n", yaxt = "n")
     axis(side = 1, at = seq(0, 30, by = 5), labels = FALSE)}
   if(position == "centre"){
-    par(mar = c(1, 1.75, 0.75, 0.75))
-    plot(x = bootData[, 1], y = bootData[, 4], type = "l", col = LColour, xlim = c(0, 60), ylim = c(0, 1),
+    par(mar = c(1, 1.75, 0.25, 0.75))
+    plot(x = bootData[, 1], y = bootData[, 4], type = "l", col = LColour, xlim = c(0, 30), ylim = c(0, 1),
          xaxt = "n", yaxt = "n")
     axis(side = 1, at = seq(0, 30, by = 5), labels = FALSE)}
   if(position == "bottom"){
     par(mar = c(1.75, 1.75, 0, 0.75))
-    plot(x = bootData[, 1], y = bootData[, 4], type = "l", col = LColour, xlim = c(0, 60), ylim = c(0, 1),
+    plot(x = bootData[, 1], y = bootData[, 4], type = "l", col = LColour, xlim = c(0, 30), ylim = c(0, 1),
          xaxt = "n", yaxt = "n")
     axis(side = 1, at = seq(0, 30, by = 5), labels = TRUE, mgp = c(0, 0, 0))
     mtext(side = 1, line = 0.75, "Dispersal distance (m)", cex = 0.6)}
@@ -527,7 +541,7 @@ ds.ccdfBoot.plot2 <- function(bootData, LColour, PColour, bottom){
 ##### [F4.3] Plot max height vs height distribution CCDF ratio --------------------------------------------
 
 # Function to plot bootstrapped CCDF, with mean line and 95% band
-ds.ccdfRatioBoot.plot2 <- function(bootData, bottom){
+ds.ccdfRatioBoot.plot2 <- function(bootData, position){
   par(cex.axis = 0.45, tcl = -0.2)
   if(position == "top"){
     par(mar = c(0.75, 1.75, 0.75, 0.75))
@@ -535,7 +549,7 @@ ds.ccdfRatioBoot.plot2 <- function(bootData, bottom){
          xaxt = "n", yaxt = "n")
     axis(side = 1, at = seq(0, 60, by = 10), labels = FALSE)}
   if(position == "centre"){
-    par(mar = c(1, 1.75, 0.75, 0.75))
+    par(mar = c(1, 1.75, 0.25, 0.75))
     plot(x = bootData[, 1], y = bootData[, 2], type = "l", xlim = c(0, 60), ylim = c(0, 4),
          xaxt = "n", yaxt = "n")
     axis(side = 1, at = seq(0, 60, by = 10), labels = FALSE)}
@@ -561,12 +575,12 @@ ds.ccdfRatioBoot.plot2 <- function(bootData, bottom){
 # For distribution, sample 100 release heights with 100 seed releases each, and repeat 1000 times
 # For mean, simulate 10000 seed release events, and repeat 1000 times
 # Either way, each bootstrap has 10000 seed release events and is repeated 1000 times
-ds.rtailBootMHD <- function(distHeight, maxHeight, Species){
-  hBoot_1 <- data.frame(replicate(1000, ds.rtail(distHeight), simplify = "matrix"))
-  hBoot_2 <- data.frame(replicate(1000, ds.rtail(maxHeight), simplify = "matrix"))
+ds.rtailBootMHD <- function(distHeight, maxHeight, species){
+  hBoot_1 <- data.frame(replicate(1000, ds.rtail(distHeight, species), simplify = "matrix"))
+  hBoot_2 <- data.frame(replicate(1000, ds.rtail(maxHeight, species), simplify = "matrix"))
   hBoot_dat <- data.frame(cbind(rep(seq(0.9500, 0.9999, by = 0.0001), 2),
                                 c(rep("Dist. Height", 500), rep("Max. Height", 500))), 
-                          rep(Species, 1000),
+                          rep(species, 1000),
                           c(apply(X = hBoot_1, MARGIN = 1, FUN = mean),
                             apply(X = hBoot_2, MARGIN = 1, FUN = mean)),
                           c(apply(X = hBoot_1, MARGIN = 1, FUN = quantile, probs = 0.025),
@@ -580,7 +594,7 @@ ds.rtailBootMHD <- function(distHeight, maxHeight, Species){
 # Plot the dispersal percentiles and the corresponding distances
 # Error bars indicate the range in which 95% of the bootstrapped simulations are
 # Lines indicate mean values
-ds.rtailBootMHD.plot <- function(rtail_data, LColour, PColour, bottom){
+ds.rtailBootMHD.plot <- function(rtail_data, LColour, PColour, position){
   rtail_data[, 1] <- as.numeric(as.character(rtail_data[, 1]))
   rtDH <- subset(rtail_data, Heights == "Dist. Height")
   rtMH <- subset(rtail_data, Heights == "Max. Height")
